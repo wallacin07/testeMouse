fd5ea480a9198aa561920e1f0f1caed139aa420d
WallyDev <160606943+wallacin07@users.noreply.github.com>
2025-03-31 09:17:38 -0300

    Update Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,10 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSignalR();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -13,18 +9,15 @@ builder.Services.AddCors(options =>
             policy.WithOrigins("http://localhost:3000")
                   .AllowAnyMethod()
                   .AllowAnyHeader()
-                  .AllowCredentials(); // 🔥 Necessário para SignalR com credenciais
+                  .AllowCredentials();
         });
 });
-
 var app = builder.Build();
 app.UseCors("AllowAll");
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.MapHub<MouseHub>("/mouse");
 app.Run();
 
